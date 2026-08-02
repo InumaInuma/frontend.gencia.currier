10f8bb93f8e41224dce56cb6ad2e2d4166932e3a
anthony.flores <anthony.flores@touring.pe>
2026-08-01 23:57:16 -0500

    feat: setup dual environment files .env.development and .env.production like FourGym

--- src/infrastructure/api/apiClient.ts
+++ src/infrastructure/api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://almaintic-001-site1.ctempurl.com',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5254',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
